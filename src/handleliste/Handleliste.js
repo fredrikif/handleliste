bc7f6e6a14c17eff033c824f119f94be60ad9126
fredrikif <fiforsa@gmail.com>
2020-05-15 12:21:53 +0200

    first handleliste commit

---
+++ src/handleliste/Handleliste.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './handleliste.css'
+import firebase from './firebase'
+import { ItemInput, onDeleteAll } from './ItemInput'
+
+function Handleliste() {
+  const [items, setItems] = React.useState([])
+  const [newItemName, setNewItemName] = React.useState('')
+
+  React.useEffect(() => {
+    const db = firebase.firestore()
+    return db.collection('handleliste').onSnapshot((snapshot) => {
+      const itemsData = []
+      snapshot.forEach((doc) => itemsData.push({ ...doc.data(), id: doc.id }))
+      setItems(itemsData)
+    })
+  }, [])
+
+  const onCreate = (e) => {
+    e.preventDefault()
+    if (newItemName !== '') {
+      const db = firebase.firestore()
+      db.collection('handleliste').add({
+        name: newItemName,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      setNewItemName('')
+    }
+  }
+
+  return (
+    <div className="app-container">
+      <h2 className="handlelisteHeading">F&M Handleliste</h2>
+      <ul className="handleliste">
+        <form className="createItem" onSubmit={onCreate}>
+          <input
+            value={newItemName}
+            onChange={(e) => setNewItemName(e.target.value)}
+          />
+          <button className="btnUpdate" type="submit">
+            <span role="img" aria-label="bacon icon">
+              🥓
+            </span>
+          </button>
+          <button className="btnDelete" onClick={onDeleteAll}>
+            <span role="img" aria-label="warning icon">
+              ⚠
+            </span>
+          </button>
+        </form>
+
+        {items
+          .sort((a, b) => b.timestamp - a.timestamp)
+          .map((item) => (
+            <li key={item.timestamp}>
+              <ItemInput item={item} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Handleliste
---
+++ src/handleliste/ItemInput.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import firebase from './firebase'
+
+export const ItemInput = ({ item }) => {
+  const [name, setName] = React.useState(item.name)
+
+  const onUpdate = () => {
+    const db = firebase.firestore()
+    db.collection('handleliste')
+      .doc(item.id)
+      .set({ ...item, name })
+  }
+
+  const onDelete = () => {
+    const db = firebase.firestore()
+    db.collection('handleliste').doc(item.id).delete()
+  }
+
+  return (
+    <>
+      <input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value)
+        }}
+      />
+
+      <button className="btnUpdate" onClick={onUpdate}>
+        <span role="img" aria-label="refresh icon">
+          ⟳
+        </span>
+      </button>
+      <button className="btnDelete" onClick={onDelete}>
+        <span role="img" aria-label="delete icon">
+          ⌫
+        </span>
+      </button>
+    </>
+  )
+}
+
+export const onDeleteAll = (e) => {
+  e.preventDefault()
+  if (window.confirm('Slette alt fra handlelista?')) {
+    const db = firebase.firestore()
+    db.collection('handleliste')
+      .get()
+      .then((res) => {
+        res.forEach((item) => {
+          item.ref.delete()
+        })
+      })
+  }
+}
