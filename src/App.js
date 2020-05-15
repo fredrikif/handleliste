8f35ed0efe26b23405f6748a39bb596dfef2ac76
fredrikif <fiforsa@gmail.com>
2020-05-15 12:19:54 +0200

    initial commit handlelisteapp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
+import { AuthProvider } from './Auth'
+import Handleliste from './handleliste/Handleliste'
+import Login from './handleliste/Login'
+// import SignUp from './handleliste/SignUp'
+import PrivateRoute from './PrivateRoute'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Handleliste} />
+          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/signup" component={SignUp} /> */}
+        </div>
+      </Router>
+    </AuthProvider>
+  )
 }
 
-export default App;
+export default App
